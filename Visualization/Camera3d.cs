9de6c39a22b8f5901815d7b2f3fafa724dffc8d1
Ktos1 <maty04188@gmail.com>
2024-11-05 20:04:15 +0100

    Make camera RTS-like

--- Visualization/Camera3d.cs
+++ Visualization/Camera3d.cs
@@ -1,30 +1,79 @@
 using Godot;
 
-public partial class Camera3d : Node3D
+public partial class Camera3d : Camera3D
 {
     [ExportGroup("Settings")]
     [Export] private float _cameraMotionSensivity = 5f;
     [Export] private float _cameraRotationSensivity = 10f;
 
     private Vector3 _direction = Vector3.Zero;
+    private Vector3 _toGlobalDirection = Vector3.Zero;
     private Vector2 _mouseInput = Vector2.Zero;
     private float _mouseInputFactor = 0.0001f;
-    private Vector3 _cameraRotation = Vector3.Zero;
+    private Vector3 _cameraRotation;
+    private bool _isCameraOnFloor = false;
+    private bool _isCameraOnCeiling = false;
+    private bool _isRotationModeOn = false;
 
     public override void _Ready()
     {
-        Input.MouseMode = Input.MouseModeEnum.Captured;
+        Input.MouseMode = Input.MouseModeEnum.Confined;
+        _cameraRotation = Rotation;
     }
 
     public override void _UnhandledInput(InputEvent @event)
     {
-        if (@event is InputEventMouseMotion)
+        if (@event is InputEventMouseMotion && _isRotationModeOn)
         {
             _mouseInput = (@event as InputEventMouseMotion).ScreenRelative;
             _mouseInput *= _mouseInputFactor * _cameraRotationSensivity;
-            _cameraRotation.X += -_mouseInput.Y;
-            _cameraRotation.Y += -_mouseInput.X;
+            _cameraRotation.X -= _mouseInput.Y;
+            _cameraRotation.Y -= _mouseInput.X;
+            Clamp(ref _cameraRotation.X, -70, -10);
             Rotation = _cameraRotation;
+        }
+        if (@event is InputEventMouseButton)
+        {
+            if (@event.IsActionPressed("zoom_in") && !_isCameraOnFloor)
+            {
+                TranslateObjectLocal(Vector3.Forward * 0.8f);
+                if (Position.Y < 2)
+                {
+                    var newPosition = Position;
+                    newPosition.Y = 2f;
+                    Position = newPosition;
+                    _isCameraOnFloor = true;
+                }
+                if (_isCameraOnCeiling)
+                {
+                    _isCameraOnCeiling = false;
+                }
+            }
+            if (@event.IsActionPressed("zoom_out") && !_isCameraOnCeiling)
+            {
+                TranslateObjectLocal(Vector3.Back * 0.8f);
+                if (Position.Y > 13)
+                {
+                    var newPosition = Position;
+                    newPosition.Y = 13f;
+                    Position = newPosition;
+                    _isCameraOnCeiling = true;
+                }
+                if (_isCameraOnFloor)
+                {
+                    _isCameraOnFloor = false;
+                }
+            }
+            if (@event.IsActionPressed("rotation_mode"))
+            {
+                Input.MouseMode = Input.MouseModeEnum.Captured;
+                _isRotationModeOn = true;
+            }
+            if (@event.IsActionReleased("rotation_mode"))
+            {
+                Input.MouseMode = Input.MouseModeEnum.Confined;
+                _isRotationModeOn = false;
+            }
         }
     }
 
@@ -40,21 +89,13 @@ public partial class Camera3d : Node3D
             {
                 _direction += Vector3.Right;
             }
-            if (@event.IsActionPressed("up"))
-            {
-                _direction += Vector3.Up;
-            }
-            if (@event.IsActionPressed("down"))
-            {
-                _direction += Vector3.Down;
-            }
             if (@event.IsActionPressed("back"))
             {
-                _direction += Vector3.Back;
+                _toGlobalDirection += Vector3.Back;
             }
             if (@event.IsActionPressed("forward"))
             {
-                _direction += Vector3.Forward;
+                _toGlobalDirection += Vector3.Forward;
             }
         }
         else
@@ -67,21 +108,13 @@ public partial class Camera3d : Node3D
             {
                 _direction -= Vector3.Right;
             }
-            if (@event.IsActionReleased("up"))
-            {
-                _direction -= Vector3.Up;
-            }
-            if (@event.IsActionReleased("down"))
-            {
-                _direction -= Vector3.Down;
-            }
             if (@event.IsActionReleased("back"))
             {
-                _direction -= Vector3.Back;
+                _toGlobalDirection -= Vector3.Back;
             }
             if (@event.IsActionReleased("forward"))
             {
-                _direction -= Vector3.Forward;
+                _toGlobalDirection -= Vector3.Forward;
             }
         }
     }
@@ -91,6 +124,27 @@ public partial class Camera3d : Node3D
         if (_direction != Vector3.Zero)
         {
             TranslateObjectLocal(_direction.Normalized() * (float)delta * _cameraMotionSensivity);
+        }
+        if (_toGlobalDirection != Vector3.Zero)
+        {
+            var globalDirection = Transform.Basis * _toGlobalDirection;
+            globalDirection.Y = 0;
+            Transform = Transform.Translated(globalDirection.Normalized() * (float)delta * _cameraMotionSensivity);
+        }
+    }
+
+    private void Clamp(ref float value, float min, float max)
+    {
+        var minInRad = min * (Mathf.Pi / 180);
+        var maxInRad = max * (Mathf.Pi / 180);
+
+        if (value < minInRad)
+        {
+            value = minInRad;
+        }
+        else if (value > maxInRad)
+        {
+            value = maxInRad;
         }
     }
 }
--- Visualization/MapObjects/Creature.cs
+++ Visualization/MapObjects/Creature.cs
@@ -3,8 +3,8 @@ using System.Diagnostics;
 
 public partial class Creature : StaticBody3D
 {
-    public override void _InputEvent(Camera3D camera, InputEvent @event, Vector3 eventPosition, Vector3 normal, int shapeIdx)
-    {
-        Debug.WriteLine("dupa");
-    }
+    //public override void _InputEvent(Camera3D camera, InputEvent @event, Vector3 eventPosition, Vector3 normal, int shapeIdx)
+    //{
+        
+    //}
 }
